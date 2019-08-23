a72a86b50600c491d4d381fe2fe84d0a5ba7d43d
sedulous-mortal <alisonnicolestuart@gmail.com>
2019-08-23 14:03:21 -0400

    trying something I found on angular university

--- src/app/my-name-form.component.ts
+++ src/app/my-name-form.component.ts
@@ -1,20 +1,23 @@
-import { Component, Input, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges, TemplateRef, ViewChild } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'my-name-form',
   template: `
+  <ng-template #nameForm>
     <mat-form-field>
       <p>Name<p>
-      <input matInput name="nameForm" #nameForm="ngModel" [(ngModel)]="name">
+      <input matInput [(ngModel)]="name">
     </mat-form-field>
+  </ng-template>
 `,
 })
 export class NameFormComponent implements OnChanges {
   @Input() name: string;
-  @ViewChild('nameForm', {static: true}) nameForm: FormGroup;
+  @ViewChild('nameForm', {static: true})
+  private nameForm: TemplateRef<any>;
 
   ngOnChanges(changes: SimpleChanges){
-    console.log(changes)
+    console.log(this.nameForm)
   }
 }
